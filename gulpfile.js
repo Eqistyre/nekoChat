ef31131f4044f5b617284268372d9493c6ee5904
Eqistyre <stillalive@aliyun.com>
2016-04-12 20:59:10 +0800

    Fix a bug
    transform * to **

--- gulpfile.js
+++ gulpfile.js
@@ -19,25 +19,46 @@ gulp.task('styles', function() {
 });
 
 gulp.task('fonts', function() {
-  gulp.src('app/fonts/*')
-      .pipe(gulp.dest('public/fonts'))
+  gulp.src('app/fonts/**')
+      .pipe(gulp.dest('public/fonts'));
+});
+
+gulp.task('images', function() {
+  gulp.src('app/images/**')
+      .pipe(gulp.dest('public/images'));
 });
 
 gulp.task('views', function() {
-  gulp.src('app/views/*')
-      .pipe(gulp.dest('views'))
+  gulp.src('app/views/**')
+      .pipe(gulp.dest('views'));
 });
+
+gulp.task('bowerFile', function() {
+  gulp.src('app/bower_components/**')
+      .pipe(gulp.dest('public/bower_components'));
+});
+
+gulp.task('scripts', () => {
+  gulp.src('app/javascripts/**/*.js')
+      .pipe($.plumber())
+      .pipe($.sourcemaps.init())
+      .pipe($.babel())
+      .pipe($.sourcemaps.write('.'))
+      .pipe(gulp.dest('public/javascripts/'))
+      .pipe(reload({stream: true}));
+});
+
 //gulp watch and reload
 gulp.watch([
-    'app/views/*.jade',
     'app/images/*',
 ]).on('change', reload);
 
+gulp.watch('app/views/*.jade', ['views']);
 gulp.watch('app/stylesheets/*.scss', ['styles']);
 gulp.watch('app/javascripts/*.js', ['scripts']);
 gulp.watch('app/fonts/**/*', ['fonts']);
 
 //extras task
-gulp.task('clean', del.bind(null, ['public/']));
+gulp.task('clean', del.bind(null, ['public/*']));
 
-gulp.task('build', ['views', 'styles', 'fonts', 'images']);
+gulp.task('build', ['clean', 'views', 'styles', 'fonts', 'images', 'bowerFile']);
